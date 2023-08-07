f33ed6b2d245ab4e18d50a03f4abf64cc151a8a7
tildetilde <tilde.egebrand@gmail.com>
2023-08-07 21:58:55 +0200

    Clean up

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-console.log("hej")
-//this is a note about what we want to do. vi ska singla slant i rundor, utfallet är krona eller klave. rundorna är x långa och y många.
-let roundCount=100000
-const roundLength=30
+// setup
+let roundCount = 100000
+const roundLength = 30
+// dom elements
+const ctx = document.getElementById('chart');
+const value = document.querySelector("#roundCountValue");
+const input = document.querySelector("#roundCountInput");
 
 function flipCoin(){
-    return Math.floor(Math.random() * 2)
+  return Math.floor(Math.random() * 2)
 }
 
 function generate(){
-    const results=Array(roundLength+1).fill(0)
-    const resultLabels=Array.from({length: roundLength+1}, (v, i) => i)
+  const results = Array(roundLength+1).fill(0)
+  const resultLabels = Array.from({length: roundLength+1}, (v, i) => i)
 
-    for(let round=0; round<roundCount; round++) {
-        let roundResult=0
-        for(let i=0; i<roundLength; i++) {
-            roundResult+=flipCoin()
-        } 
-        results[roundResult]++
+  for(let round = 0; round < roundCount; round++) {
+    let roundResult = 0
+    for(let i = 0; i < roundLength; i++) {
+      roundResult+=flipCoin()
     }
-    console.log(results)
-    const ctx = document.getElementById('myChart');
-            
-    new Chart(ctx, {
+
+    results[roundResult]++
+  }
+          
+  new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: resultLabels,
-        datasets: [{
+      labels: resultLabels,
+      datasets: [{
         label: 'Klave per runda',
         data: results,
         borderWidth: 1
-        }]
+      }]
     },
     options: {
-        scales: {
+      scales: {
         y: {
             beginAtZero: true
         }
-        }
+      }
     }
-    });
- }
+  });
+}
 
-const value = document.querySelector("#roundCountValue");
-const input = document.querySelector("#roundCountInput");
 input.value=roundCount
 generate()
 value.textContent = input.value;
