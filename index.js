6a17b762360eaf263c9177983dcd527b7e7aa83c
tildetilde <tilde.egebrand@gmail.com>
2023-08-07 22:06:47 +0200

    Update on input change

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ const roundLength = 30
 const ctx = document.getElementById('chart');
 const value = document.querySelector("#roundCountValue");
 const input = document.querySelector("#roundCountInput");
+let results = [];
+let resultLabels = [];
 
-function flipCoin(){
+function flipCoin() {
   return Math.floor(Math.random() * 2)
 }
 
-function generate(){
-  const results = Array(roundLength+1).fill(0)
-  const resultLabels = Array.from({length: roundLength+1}, (v, i) => i)
+function generateData() {
+  results = Array(roundLength+1).fill(0)
+  resultLabels = Array.from({length: roundLength+1}, (v, i) => i)
 
   for(let round = 0; round < roundCount; round++) {
     let roundResult = 0
@@ -22,32 +24,37 @@ function generate(){
 
     results[roundResult]++
   }
-          
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: resultLabels,
-      datasets: [{
-        label: 'Klave per runda',
-        data: results,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-            beginAtZero: true
-        }
-      }
-    }
-  });
 }
 
-input.value=roundCount
-generate()
+// Initial setup
+input.value = roundCount;
 value.textContent = input.value;
+generateData();
+
+const chart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: resultLabels,
+    datasets: [{
+      label: 'Klave per runda',
+      data: results,
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+          beginAtZero: true
+      }
+    }
+  }
+});
+
+// Regenerate when data is updated
 input.addEventListener("input", (event) => {
   value.textContent = event.target.value;
-  roundCount=event.target.value
-    generate()
+  roundCount = event.target.value
+  generateData()
+  chart.data.datasets[0].data = results;
+  chart.update();
 });
